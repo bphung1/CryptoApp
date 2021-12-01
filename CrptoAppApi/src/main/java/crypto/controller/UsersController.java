713afe3a2b0a748546e58d8269a9a77b93142756
Bao Phung <b.giaphung@gmail.com>
2021-12-01 13:45:51 -0600

    adding inital classes and files and folders

--- CrptoAppApi/src/main/java/crypto/controller/UsersController.java
+++ CrptoAppApi/src/main/java/crypto/controller/UsersController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class UsersController extends ControllerBase {
     @GetMapping("/login")
-    public Users login() {
-        return null;
+    public String login() {
+        return "Hello World";
     }
 
 }
