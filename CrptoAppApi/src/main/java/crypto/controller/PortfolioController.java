b34f43b05f069e48f6b4e10e522fa65e66380d17
henockb3 <henockb3@gmail.com>
2021-12-02 09:58:54 -0800

    changed return types in controller

--- CrptoAppApi/src/main/java/crypto/controller/PortfolioController.java
+++ CrptoAppApi/src/main/java/crypto/controller/PortfolioController.java
@@ -1,15 +1,16 @@
 package crypto.controller;
 
+import crypto.entity.Portfolio;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 public class PortfolioController  extends ControllerBase{
     @GetMapping("/getportfolio")
-    public String getPortfolio() {
-        return "Hello World";
+    public Portfolio getPortfolio() {
+        return null;
     }
     @PostMapping("/createportfolio")
-    public String createPortfolio() {
-        return "Hello World";
+    public Portfolio createPortfolio() {
+        return null;
     }
 }
