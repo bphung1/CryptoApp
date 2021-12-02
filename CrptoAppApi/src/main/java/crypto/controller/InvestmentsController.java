bf538ecff147d06d495f44f65937fb1adfce7abe
hatim <hatim.joubair@gmail.com>
2021-12-02 13:07:04 -0500

    fix investmenet controller

--- CrptoAppApi/src/main/java/crypto/controller/InvestmentsController.java
+++ CrptoAppApi/src/main/java/crypto/controller/InvestmentsController.java
@@ -3,12 +3,13 @@ package crypto.controller;
 
 import crypto.entity.Investment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 public class InvestmentsController extends ControllerBase {
-    @GetMapping("/getInvestments")
-    public List<Investment> getInvestments(){
+    @GetMapping("/getInvestments/{portfolioId}")
+    public List<Investment> getInvestments(@PathVariable int portfolioId){
         return null;
     }
 }
