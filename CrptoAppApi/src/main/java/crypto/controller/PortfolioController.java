a5bebad51fc2147e779731e71d5e5f18ab33f229
henockb3 <henockb3@gmail.com>
2021-12-02 10:10:31 -0800

    specified path variables

--- CrptoAppApi/src/main/java/crypto/controller/PortfolioController.java
+++ CrptoAppApi/src/main/java/crypto/controller/PortfolioController.java
@@ -2,15 +2,16 @@ package crypto.controller;
 
 import crypto.entity.Portfolio;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 public class PortfolioController  extends ControllerBase{
     @GetMapping("/{userId}/getportfolio")
-    public Portfolio getPortfolio() {
+    public Portfolio getPortfolio(@PathVariable int userId) {
         return null;
     }
     @PostMapping("/{userId}/createportfolio")
-    public Portfolio createPortfolio() {
+    public Portfolio createPortfolio(@PathVariable int userId) {
         return null;
     }
 }
