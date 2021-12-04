39fe1d15b8ebe09dded4b22b27693f96d62f464c
henockb3 <henockb3@gmail.com>
2021-12-04 09:22:05 -0800

    added update nonInvestedBalance implementaton in portfolio

--- CrptoAppApi/src/main/java/crypto/dao/PortfolioDao.java
+++ CrptoAppApi/src/main/java/crypto/dao/PortfolioDao.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface PortfolioDao {
     Portfolio createPortfolio(Portfolio portfolio);
     List<Portfolio> getPortfolio (int userId);
+    void updateNonInvestedBalance(Portfolio portfolio);
 
 //    List<Transactions> getActiveInvestment (int portfolioId);
 //    Portfolio getInvestmentBalance (int userId);
