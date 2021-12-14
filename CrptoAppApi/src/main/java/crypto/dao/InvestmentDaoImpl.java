0792a49d659c27c10773a4b5c3d0a9444b183a11
hatim <hatim.joubair@gmail.com>
2021-12-14 12:32:36 -0500

    Investment Impl

--- CrptoAppApi/src/main/java/crypto/dao/InvestmentDaoImpl.java
+++ CrptoAppApi/src/main/java/crypto/dao/InvestmentDaoImpl.java
@@ -6,6 +6,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -33,4 +34,26 @@ public class InvestmentDaoImpl implements InvestmentDao{
         final String SELECT_Investment = "SELECT * FROM Investment where portfolioId = ?;";
         return jdbcTemplate.query(SELECT_Investment, new investmentMapper(),portfolioId);
     }
+
+    @Transactional
+    @Override
+    public Investment addInvestment(int portfolioId, Investment investment) throws DataAccessException {
+        investment.setPortfolioId(portfolioId);
+        final String INSERT_NEW_INVESTMENT="INSERT INTO Investment(portfolioId,cryptoName,investedAmount,shares,cryptoRate);" +
+                "values (?,?,?,?,?)";
+        jdbcTemplate.update(INSERT_NEW_INVESTMENT,portfolioId,
+                investment.getCryptoName(),investment.getInvestedAmount(),investment.getShares(),investment.getCryptoRate());
+        int newId=jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
+        investment.setInvestmentId(newId);
+        return investment;
+    }
+
+    @Override
+    public Investment deleteInvestment(Investment investment) throws DataAccessException {
+        final String DELETE_INVESTMENT = "DELETE FROM Investment WHERE investmentId = ?;";
+        jdbcTemplate.update(DELETE_INVESTMENT,investment.getInvestmentId());
+        return investment;
+    }
+
+
 }
