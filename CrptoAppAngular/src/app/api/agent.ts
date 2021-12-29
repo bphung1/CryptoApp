43146b023cafb5cfa1ebe34782df482c4e62cddc
Bao Phung <b.giaphung@gmail.com>
2021-12-29 11:15:54 -0600

    change url to deployed url

--- CrptoAppAngular/src/app/api/agent.ts
+++ CrptoAppAngular/src/app/api/agent.ts
@@ -10,7 +10,7 @@ import { Crypto } from "../model/crypto";
     providedIn: 'root'
 })
 export class Agent {
-    url = 'http://localhost:8080/api';
+    url = 'https://cryptappl.herokuapp.com/api';
 
     public userFromAPI: Promise<User>;
     public portfolioFromAPI: Promise<Portfolio>;
